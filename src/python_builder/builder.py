0098e580b8e67ed798af974653cbe78475b5beea
kwatras <krzysztof.watras@axpo.pl>
2025-03-10 19:08:22 +0100

    limit the fields one can set to only the ones in the _allowed_fields list

--- src/python_builder/builder.py
+++ src/python_builder/builder.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, Type, TypeVar, Generic, Protocol
+from pydantic import BaseModel
 
 T = TypeVar("T")
 
@@ -9,22 +10,39 @@ class Buildable(Protocol, Generic[T]):
 
 
 class Builder(Generic[T]):
-    def __init__(self, cls: Type[T], initial_values: Dict[str, Any] = None):
+    def __init__(
+        self,
+        cls: Type[T],
+        initial_values: Dict[str, Any] = None,
+        allowed_fields: set = None,
+    ):
         self._cls = cls
         self._values = initial_values.copy() if initial_values else {}
+        self._allowed_fields = allowed_fields
+        if allowed_fields is None and issubclass(self._cls, BaseModel):
+            self._allowed_fields = set(self._cls.__fields__.keys())
+        elif allowed_fields is None and hasattr(self._cls, "__slots__"):
+            self._allowed_fields = set(self._cls.__slots__)
 
     def set(self, property_name: str, value: Any) -> "Builder[T]":
         assert isinstance(property_name, str), "property_name must be a string!"
+        if (
+            self._allowed_fields is not None
+            and property_name not in self._allowed_fields
+        ):
+            raise TypeError(
+                f'Cannot set property "{property_name}" on builder that has the "allowed_fields" enabled'
+            )
         new_values = self._values.copy()
         new_values[property_name] = value
-        return Builder(self._cls, new_values)
+        return Builder(self._cls, new_values, self._allowed_fields)
 
     def __or__(self, other: "Builder[T]") -> "Builder[T]":
         if self._cls is not other._cls:
             raise TypeError("Cannot merge builders of different classes")
         combined_values = self._values.copy()
         combined_values.update(other._values)
-        return Builder(self._cls, combined_values)
+        return Builder(self._cls, combined_values, self._allowed_fields)
 
     def build(self) -> T:
         return self._cls(**self._values)
